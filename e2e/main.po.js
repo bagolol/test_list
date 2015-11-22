75c25f38ef6edd551aa789531d8e0af8a9149c79
Rocco <roccorighi@hotmail.com>
2015-11-22 09:27:00 +0000

    adds e2e tests for error messages

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -9,10 +9,11 @@ var MainPage = function() {
   this.jumbEl = element(by.css('.jumbotron'));
   this.nameInput = element(by.css('.form-control'))
   this.namesList = element(by.css('ul.members'))
-  this.h1El = this.jumbEl.element(by.css('h1'));
-  this.imgEl = this.jumbEl.element(by.css('img'));
   this.addButton = element(by.css('.btn.btn-default'))
   this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
+  this.noNumberError = element(by.css('[ng-message=pattern]'));
+  this.fieldEmptyError = element(by.css('[ng-message=required]'));
+  this.maxLengthError = element(by.css('[ng-message=maxlength]'));
 };
 
 
--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -29,21 +29,29 @@ describe('The main view', function () {
     });
   });
 
-  describe('inputting names', function(){
+  describe('in', function(){
 
     it('should show a list of submitted names', function() {
       person.addName()
       expect(page.namesList.getText()).toBe('John');
     });
 
-    it('should not allow inputting numbers', function(){
+    it('should not allow inputting numbers and show an error', function(){
       page.nameInput.sendKeys(1);
       expect(page.addButton.isEnabled()).toBe(false);
+      expect(page.noNumberError.getText()).toBe('Your input can\'t be a number');
     });
 
     it('should not allow an empty input', function(){
       page.nameInput.sendKeys();
       expect(page.addButton.isEnabled()).toBe(false);
+      expect(page.fieldEmptyError.getText()).toBe('Input can\'t be empty...');
+    });
+
+    it('should not allow a very long name', function(){
+      page.nameInput.sendKeys('johnjohnjohnjohn');
+      expect(page.addButton.isEnabled()).toBe(false);
+      expect(page.maxLengthError.getText()).toBe('Your input is too long');
     });
 
     it('should clear the input field on submit', function(){
